4d63e428e941409f3c0460f1263388145e0a447d
Janice Samira <149676179+sami002023@users.noreply.github.com>
2023-12-12 11:49:09 +0100

    Add files via upload

---
+++ Registrationform/src/main/java/com/mycompany/registrationform/Registrationform.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registrationform;
+
+/**
+ *
+ * @author janic
+ */
+public class Registrationform {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
